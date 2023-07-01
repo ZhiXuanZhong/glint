20f3582c4c6319eeed4f163aef25d70d5ff2a949
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-02 02:22:15 +0800

    feat: finish audio messages bubble UI

--- src/components/MessageBubble/MessageBubble.tsx
+++ src/components/MessageBubble/MessageBubble.tsx
@@ -27,6 +27,15 @@ const MessageBubble = ({ message }: { message: Message }) => {
           <div className={classNames('text-xs m-1  font-semibold', message.userID === userID ? 'text-right' : null)}>{formatTime(message.timestamp)}</div>
         </div>
       )}
+      {message.type === 'audio' && (
+        <div>
+          <div className={classNames('bg-slate-300 max-w-xs rounded-lg p-3 m-1', message.userID === userID ? 'ml-auto' : null)}>
+            <div className={classNames('text-sm font-semibold', message.userID === userID ? 'hidden' : null)}>{message.username}</div>
+            <audio src={message.data} controls />
+          </div>
+          <div className={classNames('text-xs m-1  font-semibold', message.userID === userID ? 'text-right' : null)}>{formatTime(message.timestamp)}</div>
+        </div>
+      )}
     </>
   );
 };
