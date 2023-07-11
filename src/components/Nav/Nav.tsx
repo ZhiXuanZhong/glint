ed7d8a5e3ff8aeafef827fff6cb2bcb640174efb
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-11 20:58:33 +0800

    feat: update sytles for better visual coherence

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -11,12 +11,12 @@ const Nav = () => {
         <div className="ml-4 p-2">
           <GiHamburgerMenu />
         </div>
-        <div className="ml-3 mr-auto flex cursor-pointer gap-[2px] font-bold text-white">
-          <div className=" flex h-7 w-7 items-center justify-center bg-sunrise-500">G</div>
-          <div className=" flex h-7 w-7 items-center justify-center bg-sunrise-500">L</div>
-          <div className=" flex h-7 w-7 items-center justify-center bg-sunrise-500">I</div>
-          <div className=" flex h-7 w-7 items-center justify-center bg-sunrise-500">N</div>
-          <div className=" flex h-7 w-7 items-center justify-center border border-sunrise-500 text-sunrise-500">T</div>
+        <div className="ml-3 mr-auto flex cursor-pointer gap-[3px] font-bold text-white">
+          <div className="flex h-9 w-9 items-center justify-center bg-sunrise-500 text-xl">G</div>
+          <div className="flex h-9 w-9 items-center justify-center bg-sunrise-500 text-xl">L</div>
+          <div className="flex h-9 w-9 items-center justify-center bg-sunrise-500 text-xl">I</div>
+          <div className="flex h-9 w-9 items-center justify-center bg-sunrise-500 text-xl">N</div>
+          <div className="flex h-9 w-9 items-center justify-center border border-sunrise-500 text-xl text-sunrise-500">T</div>
         </div>
         {/* FIXME: logo要換 */}
         {/* <Image width="50" height="50" src={'https://placehold.co/50x50.png'} alt="logo" style={{ objectFit: 'contain' }} className="mr-auto" /> */}
