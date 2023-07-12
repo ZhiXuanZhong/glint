d76c5a1f5a15541c9fbc631d28b06396acfe9a40
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-12 15:16:51 +0800

    feat: change key of event cards to uuid

--- src/components/EventResults/EventResults.tsx
+++ src/components/EventResults/EventResults.tsx
@@ -2,7 +2,7 @@
 import EventCard from '../EventCard/EventCard';
 
 const EventResults = ({ events }: { events: Array<Event> }) => {
-  return <div className="flex flex-col gap-4">{events && events.map((event, index) => <EventCard event={event} key={index} />)}</div>;
+  return <div className="flex flex-col gap-4">{events && events.map((event, index) => <EventCard event={event} key={crypto.randomUUID()} />)}</div>;
 };
 
 export default EventResults;
