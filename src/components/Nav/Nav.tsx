3a09eb09b48851ffc3487179dba631b19c316193
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-12 15:59:13 +0800

    feat: minor update of styles

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,6 @@
 import { GiHamburgerMenu } from 'react-icons/gi';
 import NavLogin from '../NavLogin/NavLogin';
+import Link from 'next/link';
 
 const Nav = () => {
   const userID = 'rGd4NQzBRHgYUTdTLtFaUh8j8ot1';
@@ -11,12 +12,14 @@ const Nav = () => {
         <div className="ml-4 p-2">
           <GiHamburgerMenu />
         </div>
-        <div className="ml-3 mr-auto flex cursor-pointer gap-[3px] font-bold text-white">
-          <div className="flex h-9 w-9 items-center justify-center bg-sunrise-500 text-xl">G</div>
-          <div className="flex h-9 w-9 items-center justify-center bg-sunrise-500 text-xl">L</div>
-          <div className="flex h-9 w-9 items-center justify-center bg-sunrise-500 text-xl">I</div>
-          <div className="flex h-9 w-9 items-center justify-center bg-sunrise-500 text-xl">N</div>
-          <div className="flex h-9 w-9 items-center justify-center border border-sunrise-500 text-xl text-sunrise-500">T</div>
+        <div className="mr-auto">
+          <Link href={'/'} className="ml-3 flex cursor-pointer gap-[3px] font-bold text-white">
+            <div className="flex h-9 w-9 items-center justify-center bg-sunrise-500 text-xl">G</div>
+            <div className="flex h-9 w-9 items-center justify-center bg-sunrise-500 text-xl">L</div>
+            <div className="flex h-9 w-9 items-center justify-center bg-sunrise-500 text-xl">I</div>
+            <div className="flex h-9 w-9 items-center justify-center bg-sunrise-500 text-xl">N</div>
+            <div className="flex h-9 w-9 items-center justify-center border border-sunrise-500 text-xl text-sunrise-500">T</div>
+          </Link>
         </div>
         {/* FIXME: logo要換 */}
         {/* <Image width="50" height="50" src={'https://placehold.co/50x50.png'} alt="logo" style={{ objectFit: 'contain' }} className="mr-auto" /> */}
--- src/components/NavLogin/NavLogin.tsx
+++ src/components/NavLogin/NavLogin.tsx
@@ -19,9 +19,20 @@ const NavLogin = () => {
   return (
     <>
       {loaded && userID ? (
-        <div className="mr-7 flex items-center">
-          <Image width="45" height="45" src={userID.photoURL} alt="logo" style={{ objectFit: 'contain' }} className="rounded-full border border-white" />
-          <div className="ml-2">{userID.displayName}</div>
+        <div className="mr-7 flex items-center rounded-md border border-moonlight-50 px-4 py-1">
+          <Image
+            width="50"
+            height="50"
+            src={
+              'https://firebasestorage.googleapis.com/v0/b/glint-3041c.appspot.com/o/profile-image%2FrGd4NQzBRHgYUTdTLtFaUh8j8ot1_1688897462022?alt=media&token=7ed93d0e-f052-4078-aec6-8a286dd3d1cf'
+            }
+            alt="logo"
+            style={{ objectFit: 'contain' }}
+            className="rounded-full border border-white"
+          />
+          <div className="ml-2">{'倔強的小葡萄'}</div>
+          {/* <Image width="45" height="45" src={userID.photoURL} alt="logo" style={{ objectFit: 'contain' }} className="rounded-full border border-white" />
+          <div className="ml-2">{userID.displayName}</div> */}
         </div>
       ) : (
         <div>
