c86d60a3f23f8c522c558d121a30d84d0c1769d4
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-12 21:27:12 +0800

    feat: set default value for HTML date input

--- src/app/utils/formatDate.ts
+++ src/app/utils/formatDate.ts
@@ -5,12 +5,20 @@ const formatDate = (timestamp?: number) => {
     return formattedDate;
 };
 
+export const timeToHTMLInput = (timestamp: number): string => {
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+}
+
 export const formatTime = (timestamp: number): string => {
     const date = new Date(timestamp);
     let hours: string | number = date.getHours();
     let minutes: string | number = date.getMinutes();
 
-    // 添加前导零以确保两位数小时和分钟
     hours = hours.toString().padStart(2, '0');
     minutes = minutes.toString().padStart(2, '0');
 
--- src/components/SearchEvents/SearchEvents.tsx
+++ src/components/SearchEvents/SearchEvents.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { timeToHTMLInput } from '@/app/utils/formatDate';
 
 const SearchEvents = () => {
   const router = useRouter();
@@ -27,7 +28,7 @@ const SearchEvents = () => {
         <div className="flex flex-wrap p-2">
           <div className="flex p-2">
             <h2>地點</h2>
-            <select id="location">
+            <select id="location" className="pb-1 text-center">
               <option value="NEC">東北角</option>
               <option value="XL">小琉球</option>
               <option value="KT">墾丁</option>
@@ -39,7 +40,7 @@ const SearchEvents = () => {
 
           <div className="flex p-2">
             <h2>類型</h2>
-            <select name="category">
+            <select name="category" className="pb-1 text-center">
               <option value="divingTravel">潛旅</option>
               <option value="training">訓練</option>
               <option value="certificationTraining">證照課程</option>
@@ -50,17 +51,17 @@ const SearchEvents = () => {
 
           <div className="flex p-2">
             <h2>出發區間：</h2>
-            <input type="date" name="startTime" required />
+            <input type="date" name="startTime" required min={timeToHTMLInput(Date.now())} defaultValue={timeToHTMLInput(Date.now())} className="" />
           </div>
 
           <div className="flex p-2">
             <h2>至</h2>
-            <input type="date" name="endTime" required />
+            <input type="date" name="endTime" required defaultValue={timeToHTMLInput(Date.now())} className="" />
           </div>
 
           <div className="flex p-2">
             <h2>發起人</h2>
-            <select name="organizerType">
+            <select name="organizerType" className="pb-1 text-center">
               <option value="instructor">教練</option>
               <option value="diver">一般潛水員</option>
             </select>
