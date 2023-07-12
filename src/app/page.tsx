2bdac42eba9ee7ff0f6da07f3e3628edda92f639
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-12 15:01:26 +0800

    feat: update event search component button margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
       <div className="absolute left-0 top-0 h-screen w-screen bg-landing_bg bg-cover"></div>
       <div className="absolute left-0 top-0 h-screen w-screen px-60">
         <div className="pt-36 text-8xl font-bold text-white opacity-70">Dive into Adventure with</div>
-        <div className="pt-10 text-5xl font-semibold tracking-widest text-sunrise-500">GLINT</div>
+        <div className="pt-10 text-5xl font-semibold tracking-widest text-sunrise-500">SECURE</div>
       </div>
       <div className="absolute left-0 top-0 h-screen w-screen bg-landing_fg bg-cover"></div>
       <div className="relative flex h-[calc(100vh_-_5rem)] w-screen flex-col justify-end">
-        <div className="mx-auto mb-56 w-3/5 bg-white">
+        <div className="relative bottom-1/4 mx-auto w-3/5 bg-white drop-shadow-lg">
           <SearchEvents />
         </div>
       </div>
