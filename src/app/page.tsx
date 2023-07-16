8ffd72274ed81473fae39e8dc8bc7ac916cf8eb2
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-16 15:38:51 +0800

    feat: Add parallax effect to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
+import LandingBackground from '@/components/LandingBackground/LandingBackground';
 import SearchEvents from '@/components/SearchEvents/SearchEvents';
 
 export default function Home() {
   return (
     <main className="fixed left-0 z-50 h-screen w-screen">
-      <div className="absolute left-0 top-0 h-screen w-screen bg-landing_bg bg-cover"></div>
-      <div className="absolute left-0 top-0 h-screen w-screen px-60" id="#parallax">
-        <div className="pt-36 text-8xl font-bold text-white opacity-70">Dive into Adventure with</div>
-        <div className="pt-10 text-5xl font-semibold tracking-widest text-sunrise-500">SECURE</div>
-      </div>
-      <div className="absolute left-0 top-0 h-screen w-screen bg-landing_fg bg-cover"></div>
+      <LandingBackground />
       <div className="relative flex h-[calc(100vh_-_5rem)] w-screen flex-col justify-end">
         <div className="relative bottom-1/4 mx-auto w-2/4 rounded-sm border border-moonlight-50 bg-white px-3 py-1 drop-shadow-xl">
           <SearchEvents locations="NEC" category="divingTravel" organizerType="instructor" />
