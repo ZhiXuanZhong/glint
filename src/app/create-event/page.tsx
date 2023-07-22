fc4d60130fdbc0fdc3067c406e205fc3b43a8d46
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-22 22:53:55 +0800

    feat: enhance visual experience of the create event page

--- src/app/create-event/page.tsx
+++ src/app/create-event/page.tsx
@@ -1,25 +1,40 @@
 'use client';
-import { ChangeEvent, ChangeEventHandler, useEffect, useRef, useState } from 'react';
-import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
+import { ChangeEvent, ChangeEventHandler, useState } from 'react';
+import ReactDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { useRouter } from 'next/navigation';
 
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
 import { collection, doc, serverTimestamp, setDoc } from 'firebase/firestore';
+
+// 第二組：本地檔案
+import { diveSites, eventTypes } from '@/data/searchOptions';
 import db from '../utils/firebaseConfig';
 import { useAuthStore } from '@/store/authStore';
-import router, { useRouter } from 'next/navigation';
-
-interface LocationDate {
-  location: string;
-  date: string;
-}
+import startEndToTimecodes from '../utils/startEndToTimecodes';
 
 const Page = () => {
   // 這邊想做讓用戶可以增加每天對應的地點，因為可能會跳島
   const [authUser] = useAuthStore((state) => [state.authUser]);
   const [authProfile] = useAuthStore((state) => [state.authProfile]);
-  const [locationDate, setLocationDate] = useState<LocationDate[]>([{ location: '', date: '' }]);
   const [mainImage, setMainImage] = useState<any>(null);
-  const [eventID, setEventID] = useState<string | null>(null);
   const router = useRouter();
+
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(new Date());
+  const [dateRange, setDateRange] = useState<number[]>(startEndToTimecodes([new Date(), new Date()]));
+
+  const datePickerOnChange = (dates: [Date | null, Date | null]) => {
+    const [start, end] = dates;
+
+    setStartDate(start);
+    setEndDate(end);
+
+    // Only update dateRange if end date is selected
+    if (end) {
+      setDateRange(startEndToTimecodes(dates));
+    }
+  };
 
   // 這邊的type非常雜亂，gpt的產出看不懂
   const addFile: ChangeEventHandler<HTMLInputElement> = (e: ChangeEvent<HTMLInputElement>) => {
@@ -52,8 +67,8 @@ const Page = () => {
       const eventData = {
         title: formData.get('title'),
         locations: [formData.get('location')],
-        startTime: Date.parse(`${formData.get('startTime')}T00:00:00.000`),
-        endTime: Date.parse(`${formData.get('endTime')}T23:59:59.999`),
+        startTime: dateRange[0],
+        endTime: dateRange[1],
         mainImage: imageURL,
         organizer: authUser,
         organizerType: authProfile?.type,
@@ -86,69 +101,91 @@ const Page = () => {
     }
   };
 
-  // useEffect(() => {
-  //   if (eventID !== null) router.push(`/details/${eventID}`);
-  // }, []);
-
   return (
-    <form onSubmit={handleSubmit}>
-      {/* 活動名稱 */}
-      <div>活動名稱</div>
-      <input type="text" className="border" name="title" />
-      {/* 活動地點 + 時間 */}
-      <div>活動地點及時間</div>
-      {locationDate.map((fields, index) => (
-        <div key={index}>
-          <label>
-            地點
-            <select className="m-1 border" name="location">
-              <option value="NEC">東北角</option>
-              <option value="XL">小琉球</option>
-              <option value="KT">墾丁</option>
-              <option value="GI">綠島</option>
-              <option value="PH">澎湖</option>
-              <option value="LY">蘭嶼</option>
-            </select>
-          </label>
-          <label>
-            開始時間
-            <input className="m-1 border" type="date" name="startTime" />
-          </label>
-          <label>
-            結束時間
-            <input className="m-1 border" type="date" name="endTime" />
-          </label>
+    <form onSubmit={handleSubmit} className="mx-4 flex flex-col gap-5 py-10 md:mx-auto md:max-w-3xl md:p-3 lg:max-w-5xl">
+      <div className="mb-6 flex w-full flex-col md:mb-0">
+        <label className="text-lg font-medium tracking-wide text-gray-500">活動名稱</label>
+        <input type="text" className="w-full border px-4 py-2 md:max-w-[400px]" placeholder="想個吸睛的標題吧，至多20字" name="title" required maxLength={20} />
+      </div>
+
+      <div className="mb-6 flex w-full flex-col md:mb-0">
+        <label className="text-lg font-medium tracking-wide text-gray-500">參與等級建議</label>
+        <input type="text" className="w-full border px-4 py-2 md:max-w-[400px]" placeholder="請將對報名者的小提醒、要求寫在這邊，至多20字" name="levelSuggection" required maxLength={20} />
+      </div>
+
+      <div className="mb-6 flex w-full flex-col md:mb-0 md:w-[250px]">
+        <label className="text-lg font-medium tracking-wide text-gray-500">活動類型</label>
+        <div className="relative">
+          <select className="block w-full appearance-none rounded border border-gray-200 bg-gray-100 px-4 py-3 pr-8 leading-tight text-gray-700 focus:outline-none" name="category" required>
+            {eventTypes.map((type, index) => (
+              <option key={index} value={type.value}>
+                {type.label}
+              </option>
+            ))}
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
         </div>
-      ))}
-      {/* 活動類型 */}
-      <div>活動類型</div>
-      <select className="border" name="category">
-        <option value="divingTravel">潛旅</option>
-        <option value="training">訓練</option>
-        <option value="certificationTraining">證照課程</option>
-        <option value="diverWanted">找潛伴</option>
-        <option value="instructorWanted">找教練</option>
-      </select>
-      {/* 參與等級建議 */}
-      <div>參與等級建議</div>
-      <input type="text" className="border" name="levelSuggection" />
-      {/* 活動圖片 */}
-      <div>活動圖片上傳</div>
-      <input type="file" onChange={addFile} name="mainImage" />
-      {mainImage && (
-        <picture>
-          <img alt="preview image" src={URL.createObjectURL(mainImage)} className="w-96" />
-        </picture>
-      )}
-      {/* 活動詳情 */}
-      <div>活動詳情</div>
-      <textarea className="border" name="detail" />
-      <label className="block">
-        <input type="checkbox" required />
-        By signing up for our service, I agree to abide by the following terms and conditions available at Terms of Service.
-      </label>
+      </div>
+
+      <div className="relative mb-6 w-full md:mb-0 md:w-[250px]">
+        <label className="text-lg font-medium tracking-wide text-gray-500">地點</label>
+        <div className="relative">
+          <select className="block w-full appearance-none rounded border border-gray-200 bg-gray-100 px-4 py-3 pr-8 leading-tight text-gray-700 focus:outline-none" name="location" required>
+            {diveSites.map((site, index) => (
+              <option key={index} value={site.value}>
+                {site.label}
+              </option>
+            ))}
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+
+      <div className="mb-6 w-full md:mb-0 md:w-[250px]">
+        <label className="text-lg font-medium tracking-wide text-gray-500">出發區間</label>
+        <ReactDatePicker
+          selectsRange={true}
+          startDate={startDate}
+          endDate={endDate}
+          onChange={datePickerOnChange}
+          minDate={new Date()}
+          minTime={new Date(new Date().setHours(0, 0, 0, 0))}
+          isClearable={false}
+          wrapperClassName="SearchDatePicker"
+          required
+        />
+      </div>
+
+      <div className="mb-6 flex w-full flex-col md:mb-0 md:w-[250px]">
+        <label className="text-lg font-medium tracking-wide text-gray-500">活動圖片上傳</label>
+        <input type="file" accept="image/*" onChange={addFile} name="mainImage" required />
+        {mainImage && (
+          <picture>
+            <img alt="preview image" src={URL.createObjectURL(mainImage)} className="w-96" />
+          </picture>
+        )}
+      </div>
+
+      <div className="mb-6 flex w-full flex-col md:mb-0">
+        <label className="text-lg font-medium tracking-wide text-gray-500">活動詳情</label>
+        <textarea className="border p-4" name="detail" rows={10} placeholder="請填入活動內容" required />
+      </div>
+
       <div>
-        <button className=" rounded-md bg-indigo-500 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-600">建立活動</button>
+        <input type="checkbox" required />
+        <label className="ml-2">我同意遵守《服務條款》中的條款和條件。</label>
+      </div>
+
+      <div>
+        <button className="rounded-md bg-indigo-500 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-600">建立活動</button>
       </div>
     </form>
   );
