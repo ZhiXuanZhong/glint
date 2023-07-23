e8934cf4e3768817a632f388ca425abf96b62e4c
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-24 02:52:17 +0800

    refactor: update variable names

--- src/app/details/[eventID]/page.tsx
+++ src/app/details/[eventID]/page.tsx
@@ -20,13 +20,13 @@ import { convertLocationCode } from '@/app/utils/convertLocationCode';
 import { convertCategoryCode } from '@/app/utils/convertCategoryCode';
 
 export default async function Page({ params }: { params: { eventID: string } }) {
-  const eventInfos = await serverAPI.getEventInfo(params.eventID);
-  const { rating, reviewCount } = await serverAPI.getRating(eventInfos.data.organizer);
+  const eventInfo = (await serverAPI.getEventInfo(params.eventID)).data as Event;
+  const { rating, reviewCount } = await serverAPI.getRating(eventInfo.organizer);
 
   return (
     <div className="mx-4 flex h-screen flex-col p-10 md:mx-auto md:max-w-3xl lg:max-w-5xl">
       <h1 className="mb-6 border-b py-3 text-2xl font-semibold text-moonlight-900">
-        {eventInfos.data.title}
+        {eventInfo.title}
       </h1>
       <div className="flex flex-col pb-20 md:flex md:flex-row">
         <div className="md:w-2/6 md:pr-3 ">
@@ -37,12 +37,12 @@ export default async function Page({ params }: { params: { eventID: string } }) 
             </div>
             <div className="mx-auto flex w-full flex-col items-center rounded-sm  p-4 shadow-md shadow-moonlight-100">
               <div className="min-h-[80px]">
-                <UserInfo userID={eventInfos.data.organizer} size={80} />
+                <UserInfo userID={eventInfo.organizer} size={80} />
               </div>
               <div className="flex flex-col">
                 <div className="mt-2 flex w-full flex-wrap gap-3">
-                  <FollowUserButton userID={eventInfos.data.organizer} />
-                  <Link href={`/messages/${eventInfos.data.organizer}`}>
+                  <FollowUserButton userID={eventInfo.organizer} />
+                  <Link href={`/messages/${eventInfo.organizer}`}>
                     <button
                       className="w-full rounded-sm border border-transparent bg-blue-400 py-1 text-white
                     hover:bg-sunrise-600 hover:transition-all md:w-24"
@@ -69,7 +69,7 @@ export default async function Page({ params }: { params: { eventID: string } }) 
               <div>活動建議</div>
             </div>
             <div className="mt-2 w-full text-sm font-light text-moonlight-800">
-              {eventInfos.data.levelSuggection}
+              {eventInfo.levelSuggection}
             </div>
           </div>
 
@@ -81,7 +81,7 @@ export default async function Page({ params }: { params: { eventID: string } }) 
               </div>
             </div>
             <div className="mt-2 w-fit rounded-sm bg-moonlight-500 px-3 py-1 text-sm font-light text-white">
-              {convertCategoryCode(eventInfos.data.category)}
+              {convertCategoryCode(eventInfo.category)}
             </div>
           </div>
 
@@ -90,7 +90,7 @@ export default async function Page({ params }: { params: { eventID: string } }) 
               <MdLocationOn className="mr-1 text-xl" />
               <div>地點</div>
             </div>
-            {eventInfos.data.locations?.map((location: string, index: number) => (
+            {eventInfo.locations?.map((location: string, index: number) => (
               <div
                 key={index}
                 className="mt-2 w-fit rounded-sm bg-moonlight-500 px-3 py-1 text-sm font-light text-white"
@@ -105,11 +105,14 @@ export default async function Page({ params }: { params: { eventID: string } }) 
               <MdOutlineCalendarMonth className="mr-1 text-xl" />
               <div>時間</div>
             </div>
-            <StaticCalendar start={eventInfos.data.startTime} end={eventInfos.data.endTime} />
+            <StaticCalendar
+              start={new Date(eventInfo.startTime)}
+              end={new Date(eventInfo.endTime)}
+            />
           </div>
 
           <div className="mt-2 flex flex-col gap-3 border-t border-t-moonlight-50 pt-4">
-            <ApplyButton eventID={params.eventID} organizerID={eventInfos.data.organizer} />
+            <ApplyButton eventID={params.eventID} organizerID={eventInfo.organizer} />
             <FavoriteButton eventID={params.eventID} />
           </div>
         </div>
@@ -120,16 +123,16 @@ export default async function Page({ params }: { params: { eventID: string } }) 
               width={550}
               height={250}
               style={{ width: '100%', height: '250px', objectFit: 'cover' }}
-              src={eventInfos.data.mainImage}
+              src={eventInfo.mainImage}
               alt={'event picture'}
             />
           </div>
           <h2 className="mt-3 pb-3 pt-2 text-xl font-medium text-moonlight-950">活動詳情</h2>
           <div className="whitespace-pre-line py-3 text-sm leading-tight tracking-wide text-moonlight-900">
-            {eventInfos.data.description}
+            {eventInfo.description}
           </div>
           <div className="mb-16 py-3">
-            <RegistrationList eventID={params.eventID} organizerID={eventInfos.data.organizer} />
+            <RegistrationList eventID={params.eventID} organizerID={eventInfo.organizer} />
           </div>
         </div>
       </div>
