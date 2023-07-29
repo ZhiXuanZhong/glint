15e158482cdb8c05a434c5eed79c39900bfbe5e7
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-29 22:34:47 +0800

    feat: enhance search events UI results when the result is empty

--- src/components/EventResults/EventResults.tsx
+++ src/components/EventResults/EventResults.tsx
@@ -5,12 +5,16 @@ import UserInfo from '../UserInfo/UserInfo';
 const EventResults = ({ events }: { events: Array<Event> }) => {
   return (
     <div className="flex flex-col gap-4">
-      {events &&
-        events.map((event, index) => (
-          <EventCard event={event} key={crypto.randomUUID()}>
-            <UserInfo userID={event.organizer} size={80} />
-          </EventCard>
-        ))}
+      {events.length && (
+        <>
+          {events.map((event) => (
+            <EventCard event={event} key={crypto.randomUUID()}>
+              <UserInfo userID={event.organizer} size={80} />
+            </EventCard>
+          ))}
+          <div className="py-2 text-center text-sm text-gray-400">已無其他活動</div>
+        </>
+      )}
     </div>
   );
 };
