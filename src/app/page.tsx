8d6e0dc9c9475905275ff0042b4663c33ec84a51
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-29 17:11:33 +0800

    fix: update Nav z-index to prevent overlap with landing page background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SearchEvents from '@/components/SearchEvents/SearchEvents';
 
 export default function Home() {
   return (
-    <main className="fixed left-0 z-50 h-screen w-screen">
+    <main className="fixed left-0 z-20 h-screen w-screen">
       <LandingBackground />
       <div className="relative flex h-[calc(100vh_-_5rem)] w-screen flex-col justify-end">
         <div className="relative bottom-1/4 mx-auto w-2/4 rounded-sm border border-moonlight-50 bg-white px-3 py-1 drop-shadow-xl">
--- src/components/NavLogin/NavLogin.tsx
+++ src/components/NavLogin/NavLogin.tsx
@@ -15,8 +15,14 @@ import ProfileButton from '../ProfileButton/ProfileButton';
 const NavLogin = () => {
   const [loaded, setLoaded] = useState(false);
   const [open, setOpen] = useState(false);
-  const [authUser, updateAuthUser] = useAuthStore((state) => [state.authUser, state.updateAuthUser]);
-  const [authProfile, updateAuthProfile] = useAuthStore((state) => [state.authProfile, state.updateAuthProfile]);
+  const [authUser, updateAuthUser] = useAuthStore((state) => [
+    state.authUser,
+    state.updateAuthUser,
+  ]);
+  const [authProfile, updateAuthProfile] = useAuthStore((state) => [
+    state.authProfile,
+    state.updateAuthProfile,
+  ]);
 
   const handleLogout = () => {
     const auth = getAuth(app);
@@ -44,13 +50,31 @@ const NavLogin = () => {
             className="relative mr-7 flex cursor-pointer items-center rounded-sm border border-moonlight-50 px-4 py-1 shadow-sm transition-colors hover:border-moonlight-400 hover:bg-gray-100"
             onClick={() => setOpen((prev) => !prev)}
           >
-            <Image width="50" height="50" src={authProfile?.avatarURL} alt="logo" style={{ objectFit: 'contain' }} className="rounded-full border border-white" />
+            <Image
+              width="50"
+              height="50"
+              src={authProfile?.avatarURL}
+              alt="logo"
+              style={{ objectFit: 'contain' }}
+              className="rounded-full border border-white"
+            />
             <div className="mx-2 line-clamp-1">{authProfile?.username}</div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="h-4 w-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2.5}
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
               <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
             </svg>
 
-            <ul className={`absolute right-0 top-[60px] w-fit rounded-sm bg-white shadow-xl ${open ? 'block' : 'hidden'}`}>
+            <ul
+              className={`absolute right-0 top-[60px] w-fit rounded-sm bg-white shadow-xl ${
+                open ? 'block' : 'hidden'
+              }`}
+            >
               <Link href={`/profile/${authUser}`}>
                 <li className="flex w-full items-center justify-end px-3 py-4 text-center text-[17px] hover:bg-gray-100">
                   <div className="mr-2 text-3xl md:text-xl">
@@ -59,7 +83,10 @@ const NavLogin = () => {
                   個人檔案
                 </li>
               </Link>
-              <li className="flex w-full items-center justify-end px-3 py-4 text-center hover:bg-gray-100" onClick={handleLogout}>
+              <li
+                className="flex w-full items-center justify-end px-3 py-4 text-center hover:bg-gray-100"
+                onClick={handleLogout}
+              >
                 <div className="mr-2 text-3xl md:text-xl">
                   <ImExit />
                 </div>
