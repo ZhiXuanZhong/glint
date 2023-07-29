bf0d16ce1bdbdc44d43f0eb8135d99f0afd6bbf0
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-29 16:16:34 +0800

    feat: add search for all events option to enhance user experience

--- src/app/utils/paramsToFireConditions.ts
+++ src/app/utils/paramsToFireConditions.ts
@@ -1,40 +1,40 @@
 import { where, QueryFieldFilterConstraint } from 'firebase/firestore';
 
 const paramsToFireConditions = (queryParams: QueryParams) => {
-    const queryConditions: QueryFieldFilterConstraint[] = [];
-    Object.keys(queryParams).reduce((acc: QueryConditions[], key: string) => {
-        const value = queryParams[key];
-        if (value && value !== 'null') {
-            let operator: QueryConditions['operator'];
-            switch (key) {
-                case 'locations':
-                    operator = 'array-contains';
-                    break;
+  const queryConditions: QueryFieldFilterConstraint[] = [];
+  Object.keys(queryParams).reduce((acc: QueryConditions[], key: string) => {
+    const value = queryParams[key];
+    if (value && value !== 'null' && value !== 'all') {
+      let operator: QueryConditions['operator'];
+      switch (key) {
+        case 'locations':
+          operator = 'array-contains';
+          break;
 
-                case 'startTime':
-                    operator = '>=';
-                    break;
+        case 'startTime':
+          operator = '>=';
+          break;
 
-                case 'endTime':
-                    queryConditions.push(where('startTime', '<=', value));
-                    return acc;
+        case 'endTime':
+          queryConditions.push(where('startTime', '<=', value));
+          return acc;
 
-                default:
-                    operator = '==';
-                    break;
-            }
+        default:
+          operator = '==';
+          break;
+      }
 
-            acc.push({
-                property: key,
-                operator: operator,
-                value: value,
-            });
+      acc.push({
+        property: key,
+        operator: operator,
+        value: value,
+      });
 
-            queryConditions.push(where(key, operator, value));
-        }
-        return acc;
-    }, []);
-    return queryConditions
-}
+      queryConditions.push(where(key, operator, value));
+    }
+    return acc;
+  }, []);
+  return queryConditions;
+};
 
-export default paramsToFireConditions
+export default paramsToFireConditions;
