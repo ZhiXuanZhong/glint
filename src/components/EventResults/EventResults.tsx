66982ff77068ddb8ec80ba2d4acb99d7171b5476
ZhiXuan Zhong <dooi921@gmail.com>
2023-07-29 23:55:55 +0800

    feat: add test account information

--- src/components/EventResults/EventResults.tsx
+++ src/components/EventResults/EventResults.tsx
@@ -5,7 +5,7 @@ import UserInfo from '../UserInfo/UserInfo';
 const EventResults = ({ events }: { events: Array<Event> }) => {
   return (
     <div className="flex flex-col gap-4">
-      {events.length && (
+      {events.length ? (
         <>
           {events.map((event) => (
             <EventCard event={event} key={crypto.randomUUID()}>
@@ -14,7 +14,7 @@ const EventResults = ({ events }: { events: Array<Event> }) => {
           ))}
           <div className="py-2 text-center text-sm text-gray-400">已無其他活動</div>
         </>
-      )}
+      ) : null}
     </div>
   );
 };
