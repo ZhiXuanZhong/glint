90a09ff36989d6cb8c20c63161eab0667b4a2103
ZhiXuan Zhong <dooi921@gmail.com>
2023-08-13 13:17:56 +0800

    feat: update comments in the startEndToTimecodes

--- src/app/utils/startEndToTimecodes.ts
+++ src/app/utils/startEndToTimecodes.ts
@@ -1,11 +1,10 @@
-// 將dateRange轉換成timecode，開頭00:00結束23:59
 const startEndToTimecodes = ([startDate, endDate]: [Date | null, Date | null]): number[] => {
-    const start = startDate ? new Date(startDate) : new Date();
-    start.setHours(0, 0, 0);
-    const end = endDate ? new Date(endDate) : new Date();
-    end.setHours(23, 59, 59);
+  const start = startDate ? new Date(startDate) : new Date();
+  start.setHours(0, 0, 0);
+  const end = endDate ? new Date(endDate) : new Date();
+  end.setHours(23, 59, 59);
 
-    return [start.getTime(), end.getTime()];
+  return [start.getTime(), end.getTime()];
 };
 
 export default startEndToTimecodes;
